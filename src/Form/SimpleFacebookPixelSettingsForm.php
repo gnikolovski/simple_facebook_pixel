afa692a991d23fecf982a3f9aa27abcc7a50fe7f
Goran Nikolovski <nikolovski84@gmail.com>
2019-04-04 20:55:11 +0200

    Add ViewContent settings

--- src/Form/SimpleFacebookPixelSettingsForm.php
+++ src/Form/SimpleFacebookPixelSettingsForm.php
@@ -38,7 +38,18 @@ class SimpleFacebookPixelSettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('pixel_enabled'),
     ];
 
-    $form['pixel_id'] = [
+    $form['basic_settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Basic settings'),
+      '#open' => FALSE,
+      '#states' => [
+        'visible' => [
+          ':input[name="pixel_enabled"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['basic_settings']['pixel_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Facebook Pixel ID'),
       '#default_value' => $config->get('pixel_id'),
@@ -50,11 +61,44 @@ class SimpleFacebookPixelSettingsForm extends ConfigFormBase {
       ],
     ];
 
-    $form['excluded_roles'] = [
+    $form['basic_settings']['excluded_roles'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Exclude Facebook Pixel for the following roles'),
       '#options' => array_map('\Drupal\Component\Utility\Html::escape', user_role_names()),
       '#default_value' => $config->get('excluded_roles'),
+      '#states' => [
+        'visible' => [
+          ':input[name="pixel_enabled"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['events'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Events'),
+      '#open' => FALSE,
+      '#states' => [
+        'visible' => [
+          ':input[name="pixel_enabled"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['events']['notice'] = [
+      '#type' => 'markup',
+      '#markup' => '<p>' . $this->t('PageView event is by default enabled on all pages. Other events can be enabled/disabled bellow.') . '</p>',
+      '#states' => [
+        'visible' => [
+          ':input[name="pixel_enabled"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['events']['view_content_entities'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Add ViewContent event to the following pages'),
+      '#options' => array_map('\Drupal\Component\Utility\Html::escape', $this->getViewContentEntities()),
+      '#default_value' => $config->get('view_content_entities'),
       '#states' => [
         'visible' => [
           ':input[name="pixel_enabled"]' => ['checked' => TRUE],
@@ -75,9 +119,38 @@ class SimpleFacebookPixelSettingsForm extends ConfigFormBase {
       ->set('pixel_enabled', $values['pixel_enabled'])
       ->set('pixel_id', $values['pixel_id'])
       ->set('excluded_roles', $values['excluded_roles'])
+      ->set('view_content_entities', $values['view_content_entities'])
       ->save();
 
     parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Gets content entities for which it is possible to enable ViewContent event.
+   *
+   * @return array
+   *   The list of entities.
+   */
+  protected function getViewContentEntities() {
+    $result = [];
+
+    $content_types = \Drupal::entityTypeManager()
+      ->getStorage('node_type')
+      ->loadMultiple();
+
+    foreach ($content_types as $content_type) {
+      $result['node:' . $content_type->getOriginalId()] = $this->t('Node') . ': ' . $content_type->label();
+    }
+
+    $vocabularies = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_vocabulary')
+      ->loadMultiple();
+
+    foreach ($vocabularies as $vocabulary) {
+      $result['taxonomy_term:' . $vocabulary->getOriginalId()] = $this->t('Taxonomy') . ': ' . $vocabulary->label();
+    }
+
+    return $result;
   }
 
 }
