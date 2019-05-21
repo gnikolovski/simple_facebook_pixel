ed25c96df6dde7ac35c2dae0f1cfe42674269dfc
goran_pds <goran_pds@3451979.no-reply.drupal.org>
2019-05-21 20:20:55 +0200

    Issue #3050594 by gnikolovski: Add AddToCart event

--- src/EventSubscriber/PixelSubscriber.php
+++ src/EventSubscriber/PixelSubscriber.php
@@ -3,6 +3,7 @@
 namespace Drupal\simple_facebook_pixel\EventSubscriber;
 
 use Drupal\Core\Cache\Cache;
+use Drupal\simple_facebook_pixel\PixelBuilderService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -15,22 +16,61 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class PixelSubscriber implements EventSubscriberInterface {
 
   /**
+   * The Pixel builder.
+   *
+   * @var \Drupal\simple_facebook_pixel\PixelBuilderService
+   */
+  protected $pixelBuilder;
+
+  /**
+   * PixelSubscriber constructor.
+   *
+   * @param \Drupal\simple_facebook_pixel\PixelBuilderService $pixel_builder
+   *   The Pixel builder.
+   */
+  public function __construct(PixelBuilderService $pixel_builder) {
+    $this->pixelBuilder = $pixel_builder;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::RESPONSE][] = ['onResponse'];
+    $events[KernelEvents::RESPONSE][] = ['onKernelResponse'];
+    $events['commerce_cart.entity.add'][] = ['addToCartEvent'];
     return $events;
   }
 
   /**
    * Invalidates page cache tags if needed.
    */
-  public function onResponse(FilterResponseEvent $event) {
+  public function onKernelResponse(FilterResponseEvent $event) {
     $response = $event->getResponse();
 
     if (strpos($response->getContent(), 'CompleteRegistration') !== FALSE) {
       Cache::invalidateTags(['simple_facebook_pixel:complete_registration']);
     }
+
+    if (strpos($response->getContent(), '"track", "AddToCart"') !== FALSE) {
+      Cache::invalidateTags(['simple_facebook_pixel:add_to_cart']);
+    }
+  }
+
+  /**
+   * @param $event
+   */
+  public function addToCartEvent($event) {
+    $product_variation = $event->getEntity();
+
+    $data = [
+      'content_name' => $product_variation->getProduct()->getTitle(),
+      'content_type' => 'product',
+      'content_ids' => [$product_variation->getSku()],
+      'value' => $product_variation->getPrice()->getNumber(),
+      'currency' => $product_variation->getPrice()->getCurrencyCode(),
+    ];
+
+    $this->pixelBuilder->addEvent('AddToCart', $data, TRUE);
   }
 
 }
